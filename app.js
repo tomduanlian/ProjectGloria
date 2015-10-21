a63ec910a9c961dd6b54072baac32ac9ee8c69eb
tomduanlian <tom.duanlian@gmail.com>
2015-10-21 14:15:55 -0700

    modify app.js for adding mongoose, passport and passport-local strategy

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash = require('connect-flash');
+var LocalStrategy = require('passport-local').Strategy;
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
