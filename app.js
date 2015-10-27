4bbcf6f6f6963b1284289b8e33d73aaecebadec4
tomduanlian <tom.duanlian@gmail.com>
2015-10-27 11:10:30 -0700

    remove unused comment on app.js

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ mongoose.connect('mongodb://localhost/project_gloria');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public/images', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
